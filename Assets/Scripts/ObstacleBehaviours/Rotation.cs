0f9db4c8e6b18894889cd0ed7e8f8f9ce811fa28
LineCab <line.cabaret@mmibordeaux.com>
2024-04-23 17:18:02 +0200

    ajout camera

--- Assets/Scripts/ObstacleBehaviours/Rotation.cs
+++ Assets/Scripts/ObstacleBehaviours/Rotation.cs
@@ -9,7 +9,7 @@ public class Rotation : MonoBehaviour {
 	[SerializeField] private float angle = 90f; //Au bout de quel angle parcouru il peut marquer une pause
 	[SerializeField] private bool sensHoraire = true; //S'il tourne dans le sens horaire ou non
 	
-	private int sens = -1;
+	private int sens = 1;
 	private float timer = 0; //timer du temps de pause
 	private float timerAngle = 0; //si l'objet marque une pause au bout d'un certain angle, initialisé à la valeur de l'angle, si ça arrive à 0, marque une pause
 	private bool angleRectify = false;
@@ -19,26 +19,18 @@ public class Rotation : MonoBehaviour {
 	void Start() {
 		timerAngle = angle;
 		timer = pause;
-		if(!sensHoraire){
-			sens = 1;
-		}
 	}
 
 	// Update is called once per frame
 	void FixedUpdate() {
-		if(timerAngle == 0 && pause != 0){ //Si le timer de l'angle vaut 0 mais que l'objet est en pause (pause non égal à 0)
-			if(!angleRectify){ //Si c'est la première fois qu'il est en pause, on réajuste l'angle de l'objet à sa position d'angle exacte pour éviter des micro-décalages qui s'accumulent
-				cnt = (cnt+1)% (int)(360/angle);
-				angleRectify = true;
-				Quaternion target = Quaternion.Euler(0, 0, angle * cnt * sens);
-				transform.rotation = target;
-			}
+		if(timerAngle == 0 && pause != 0){ 
 			if(timer > 0){ //Si le timer est supérieur à 0, on enlève le laps de temps écoulé Time.fixedDeltaTime au timer (sans dépasser 0, grâce à la fonction max)
 				timer = Mathf.Max(0, timer - Time.fixedDeltaTime);
 			} else { //Si le timer est égal à 0, on réinitialise timerAngle à la valeur de angle et timer à la valeur de pause, on remet angleRectify à false
 				timer = pause;
 				timerAngle = angle;
 				angleRectify = false;
+				sens = sens * (-1);
 			}
 		} else { //Si l'objet n'est pas en pause, on le fait tourner via son transform. On soustrait l'angle parcouru en une frame au timerAngle en valeur absolu pour ne pas dépendre du sens de rotation (sans dépasser 0, grâce à la fonction max)
 			transform.Rotate(0, 0, 20 * sens * speed * Time.fixedDeltaTime);
