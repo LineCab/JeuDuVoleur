1cdc1a4ecc336751d053923bb119111a424b2122
Simon Daguet-Kargl <simondaguetkargl@gmail.com>
2024-04-24 11:26:42 +0200

    laser début

---
+++ Assets/Scripts/ObstacleBehaviours/Laser.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+	// classe qui gère le comportement du laser
+	[SerializeField] private float speed = 1f; // vitesse du laser
+	// taille de fin du laser
+	[SerializeField] private float endSize = 1f;
+	// taille de départ du laser
+	[SerializeField] private float startSize = 1f;
+	// actualiser la taille du laser
+	// taille de départ du laser
+	void Start()
+	{
+		// taille de laser de 1y 1x 1z
+		transform.localScale = new Vector3(1, 1, 0);
+	}
+	void Update()
+	{
+		// actualiser la taille du laser
+	}
+}
