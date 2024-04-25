9b1e7bed92e530e154b920e66535692a8c9c2bd2
LineCab <line.cabaret@mmibordeaux.com>
2024-04-25 12:42:10 +0200

    Modif policier

--- Assets/Scripts/ObstacleBehaviours/FollowingPlayerMove.cs
+++ Assets/Scripts/ObstacleBehaviours/FollowingPlayerMove.cs
@@ -7,10 +7,9 @@ public class FollowingPlayerMove : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f; // Vitesse de l'objet, modifiable
     [SerializeField] private Rigidbody2D rb; // Le rigidbody pour bouger l'obstacle
     private GameObject player;
-
     private float delay = 0.5f;
     private float timerDelay = 0;
-    private Vector2 initPosition; // Position initiale
+    private Vector2 initPosition; // position initiale
 
     // Au démarrage, défini la variable de mouvement
     void Start()
@@ -18,13 +17,6 @@ public class FollowingPlayerMove : MonoBehaviour
         player = PlayerManager.GetPlayer();
         initPosition = new Vector2(transform.position.x, transform.position.y);
     }
-
-	// Fonction qui remet l'objet à sa position initiale
-	public void reinitPosition()
-	{
-		transform.position = initPosition;
-		timerDelay += delay;
-	}
 
     // A chaque frame, on bouge l'objet via son rigidbody dans le mouvement défini * la vitesse de l'objet moveSpeed * Time.fixedDeltaTime le laps de temps écoulé en 1 frame
     void FixedUpdate()
@@ -38,11 +30,17 @@ public class FollowingPlayerMove : MonoBehaviour
         // Calculer la direction vers le joueur
         Vector2 direction = (player.transform.position - transform.position).normalized;
 
-        // Orienter l'objet vers 0 degrés
-        transform.rotation = Quaternion.Euler(0, 0, 0);
+        // Orienter l'objet vers le joueur
+        transform.right = direction;
 
         // Déplacer l'objet dans la direction du joueur
         rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
     }
 
+    // Fonction qui remet l'objet à sa position initiale
+    public void reinitPosition()
+    {
+        transform.position = initPosition;
+        timerDelay += delay;
+    }
 }
